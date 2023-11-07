78cb4a319b85cfd781d9b2be8b88fa80e94a9ab3
Carina Villasanti <carina-villasanti@mdh.gob.ar>
2023-11-07 13:55:32 -0300

    second updte

--- bd_tarea_obligatoria.sql
+++ bd_tarea_obligatoria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2023 a las 17:09:05
+-- Tiempo de generación: 07-11-2023 a las 17:49:40
 -- Versión del servidor: 8.0.35
 -- Versión de PHP: 8.2.4
 
@@ -35,6 +35,17 @@ CREATE TABLE `personas` (
   `fecha` timestamp NOT NULL,
   `provincia` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombre`, `apellido`, `edad`, `fecha`, `provincia`) VALUES
+(1, 'Carina', 'Villasanti', 39, '2023-11-07 16:48:00', 'Formosa'),
+(2, 'Carolina', 'Figueredo', 25, '2023-11-07 16:48:19', 'Resistencia'),
+(3, 'Roberto', 'Oviedo', 36, '2023-11-07 16:48:33', 'Buenos Aires'),
+(4, 'Fabian', 'Franco', 20, '2023-11-07 16:48:44', 'Chubut'),
+(5, 'Roxana', 'Garcete', 45, '2023-11-07 16:48:53', 'Cordoba');
 
 --
 -- Índices para tablas volcadas
